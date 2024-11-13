a694d2c6199f48f30530e2a4c274a990cea8098d
Andrej Mjeda <andrej.mjeda@gmail.com>
2024-11-13 18:14:27 +0100

    zoo dijagram

--- zoo.sql
+++ zoo.sql
@@ -3,13 +3,13 @@
 use master;
 go
 
-drop database if exists edunovawp6;
+drop database if exists zoo;
 go
 
-create database edunovawp6;
+create database zoo;
 go
 
-use edunovawp6;
+use zoo;
 go
 
 --create database zoo;
@@ -27,27 +27,31 @@ go
 
 --drop table zivotinja;
 
-create table zivotinja(
-sifra int not null primary key identity(1,1),
-vrsta varchar(50),
-ime varchar(50),
-djelatnik int references djelatnik(sifra),
-prostorija int references prostorija(sifra),
-datum int references datum(sifra)
-);
-
-drop table zivotinja;
-
 create table djelatnik(
 sifra int not null primary key identity(1,1),
-ime varchar(50),
-prezime varchar(50),
+ime varchar(50) not null,
+prezime varchar(50) not null,
 iban varchar(50)
 );
 
 create table datum(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
-iban varchar(50)
+sifra int not null primary key identity(1,1),
+datumrodenja datetime,
+datumdolaska datetime
+);
+
+create table prostorija(
+sifra int not null primary key identity(1,1),
+dimenzije varchar(30) not null,
+maks_broj int not null,
+mjesto varchar(30)
+);
+
+create table zivotinja(
+sifra int not null primary key identity(1,1),
+vrsta varchar(50) not null,
+ime varchar(50) not null,
+djelatnik int not null references djelatnik(sifra),
+prostorija int not null references prostorija(sifra),
+datum int not null references datum(sifra)
 );
