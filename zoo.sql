a33ebccc044d9b4219675d5d6736331b5da5158c
Andrej Mjeda <andrej.mjeda@gmail.com>
2024-11-13 17:59:02 +0100

    rad na zoo bazi

--- zoo.sql
+++ zoo.sql
@@ -1,5 +1,17 @@
 --use edunovawp6;
 
+use master;
+go
+
+drop database if exists edunovawp6;
+go
+
+create database edunovawp6;
+go
+
+use edunovawp6;
+go
+
 --create database zoo;
 
 --use zoo;
@@ -14,3 +26,28 @@
 --);
 
 --drop table zivotinja;
+
+create table zivotinja(
+sifra int not null primary key identity(1,1),
+vrsta varchar(50),
+ime varchar(50),
+djelatnik int references djelatnik(sifra),
+prostorija int references prostorija(sifra),
+datum int references datum(sifra)
+);
+
+drop table zivotinja;
+
+create table djelatnik(
+sifra int not null primary key identity(1,1),
+ime varchar(50),
+prezime varchar(50),
+iban varchar(50)
+);
+
+create table datum(
+sifra int,
+ime varchar(50),
+prezime varchar(50),
+iban varchar(50)
+);
