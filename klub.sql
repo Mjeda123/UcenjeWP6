52fa24c198e525458e3b988786a2228daeb757ae
Mjeda123 <andrej.mjeda@gmail.com>
2024-11-14 15:11:49 +0100

    klub dijagram

--- klub.sql
+++ klub.sql
@@ -11,14 +11,42 @@ prezime varchar(50) not null,
 datum_rodenja varchar(50),
 pozicija varchar(50) not null,
 bro_dresa varchar(50) not null,
-klub int not null
+klub int not null references klub(sifra)
 );
+
+drop table igrac;
 
 create table trener(
 sifra int not null primary key identity(1,1),
 ime varchar(50) not null,
 prezime varchar(50) not null,
-klub varchar(50),
+klub int not null references klub(sifra),
 nacionalnost varchar(50) not null,
 iskustvo varchar(50) not null
 );
+
+drop table trener;
+
+create table utakmica(
+sifra int not null primary key identity(1,1),
+datum time not null,
+vrijeme date not null,
+lokacija varchar(50) not null,
+stadion varchar(50) not null,
+domaci_klub int not null references klub(sifra),
+gostujuci_klub int not null references klub(sifra)
+);
+
+drop table utakmica;
+
+create table klub(
+sifra int not null primary key identity(1,1),
+naziv int not null,
+osnovan varchar(50),
+stadion int not null,
+predsjednik varchar(50) not null,
+drzava int not null,
+liga varchar(50) not null
+);
+
+drop table klub;
