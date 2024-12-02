7a6c7b9e7b2d8e3cdf5961b425298cb2d2068686
bcacic <101899269+Mjeda123@users.noreply.github.com>
2024-12-02 18:51:27 +0100

    UlazIzlaz

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,9 +1,14 @@
-﻿//Ovo je komentar jedna linija
+﻿using Ucenje;
+
+E01UlazIzlaz.Izvedi();
+
+
+//Ovo je komentar jedna linija
 
 /*
  * Komentar više linija
  */
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav Svijetu ČŠĆDŽ čšćdž");
-Console.WriteLine("Hello Osijek");
+//Console.WriteLine("Pozdrav Svijetu ČŠĆDŽ čšćdž");
+//Console.WriteLine("Hello Osijek");
