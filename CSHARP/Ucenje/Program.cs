9778f797327b4ea6923e9e459aa068e63c5e2f36
bcacic <101899269+Mjeda123@users.noreply.github.com>
2024-12-02 17:51:22 +0100

    Upoznavanje s sintaksom

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ovo je komentar jedna linija
+
+/*
+ * Komentar više linija
+ */
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pozdrav Svijetu ČŠĆDŽ čšćdž");
+Console.WriteLine("Hello Osijek");
