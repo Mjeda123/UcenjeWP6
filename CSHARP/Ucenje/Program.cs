c9ed5a08cbc702f70b1a9665ac487766f41b00a9
Andrej Mjeda <andrej.mjeda@gmail.com>
2025-01-20 18:00:09 +0100

    Generičke klase, Sučelja, Atributi

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -2,7 +2,9 @@
 
 using Ucenje;
 
-new Ucenje.E18NasljedivanjePolimorfizam.Program("E18");
+new Ucenje.E19GenericiLambdaEkstenzije.Program();
+
+//new Ucenje.E18NasljedivanjePolimorfizam.Program("E18");
 
 //new Ucenje.E17KlasaObjekt.Program();
 
