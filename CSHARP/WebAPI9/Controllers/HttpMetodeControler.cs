20c84da3f9cdf83119d0b500ef30631a3e13a405
Andrej Mjeda <andrej.mjeda@gmail.com>
2025-01-24 18:53:19 +0100

    nastava

---
+++ CSHARP/WebAPI9/Controllers/HttpMetodeControler.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI9.Models;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace WebAPI9.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+
+    public class HttpMetodeController : ControllerBase
+    {
+        [HttpGet]
+
+        public string HelloWorld()
+        {
+            return "Hello {ime}!";
+        }
+
+
+        [HttpGet]
+        [Route("helloworld")]
+        public string HelloWorld(string ime)
+        {
+            return $"Hello World!";
+
+        }
+
+        [HttpGet]
+        [RouteAttribute("json")]
+        public IActionResult Json(int sifra, string ime)
+        {
+            return Ok(new { Sifra = sifra, Ime = ime });
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(Osoba osoba)
+        {
+            osoba.Ime = "Hello " + osoba.Ime;
+            return StatusCode(201, osoba);
+        }
+
+
+        [Httpput]
+        public IActionResult Put(Osoba osoba)
+        {
+            osoba.Ime = "Hello" + osoba.Ime;
+            return StatusCode(StatusCodes.Status206PartialContent, osoba);
+
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int sifra)
+        {
+            if (sifra <= 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { poruka = "Sifra mora biti veća od 0" });
+            }
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+    }
+
+
+
+    
+    }
